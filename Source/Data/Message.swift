187f31a5cfe77f0beffe88d0ad62b464adec90ed
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-17 19:19:21 +0300

    Added Message model

---
+++ Source/Data/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Reportage
+//
+//  Created by Igor Matyushkin on 17.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public struct Message {
+    var prefix: String
+    var text: String = ""
+    var uppercased: Bool = false
+}
