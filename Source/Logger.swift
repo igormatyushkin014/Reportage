882e2459356fc720fd8c3246770ebf828af50001
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-17 13:31:41 +0300

    Added library

---
+++ Source/Logger.swift
@@ -0,0 +1,35 @@
+//
+//  Logger.swift
+//  Reportage
+//
+//  Created by Igor Matyushkin on 17.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public class Logger {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init() {
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
