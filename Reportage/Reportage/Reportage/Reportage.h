52cfe86a8a93f3bf04975ba7cbd4b1736aad4598
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-17 13:31:51 +0300

    Added demo projet

---
+++ Reportage/Reportage/Reportage/Reportage.h
@@ -0,0 +1,19 @@
+//
+//  Reportage.h
+//  Reportage
+//
+//  Created by Igor Matyushkin on 17.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reportage.
+FOUNDATION_EXPORT double ReportageVersionNumber;
+
+//! Project version string for Reportage.
+FOUNDATION_EXPORT const unsigned char ReportageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reportage/PublicHeader.h>
+
+
---
+++ Reportage/ReportageDemo/ReportageDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  ReportageDemo
+//
+//  Created by Igor Matyushkin on 17.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupNavigationBar()
+    }
+    
+    // MARK: Private object methods
+    
+    fileprivate func setupNavigationBar() {
+        self.navigationItem.title = "Main"
+    }
+    
+    // MARK: Actions
+    
+}
