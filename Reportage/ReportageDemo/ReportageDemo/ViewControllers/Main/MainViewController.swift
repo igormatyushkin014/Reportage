a9129ca54171d4635bb79af35fc59552beb3baef
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-17 21:24:15 +0300

    Updated demo project

--- Reportage/ReportageDemo/ReportageDemo/ViewControllers/Main/MainViewController.swift
+++ Reportage/ReportageDemo/ReportageDemo/ViewControllers/Main/MainViewController.swift
@@ -45,12 +45,14 @@ class MainViewController: UIViewController {
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         
         Logger.shared
-            .width(90)
-            .offset(0)
+            .width(80)
+            .offset(4)
             .offsetPattern(" ")
             .prefix("🚀")
             .dateFormat(.dateTime)
             .uppercased(false)
+            .header(.none)
+            .footer(.emptyLine)
             .print(text)
     }
     
