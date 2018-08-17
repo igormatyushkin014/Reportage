1fd379837bad86c6f5ad468568ff46d4fa82aefe
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-17 19:20:15 +0300

    Added DateFormat enumeration

---
+++ Source/Types/DateFormat.swift
@@ -0,0 +1,15 @@
+//
+//  DateFormat.swift
+//  Reportage
+//
+//  Created by Igor Matyushkin on 17.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum DateFormat {
+    case full
+    case short
+    case none
+}
